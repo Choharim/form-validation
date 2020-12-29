0dcbaf34ebcf7a492c6479a65a18a4c51b4efc55
Choharim <jhl8586@naver.com>
2020-12-29 16:15:35 +0900

    Add firstName,lastName,email

--- src/containers/FormUserDetails.js
+++ src/containers/FormUserDetails.js
@@ -1,11 +1,80 @@
 import React from "react";
+import styled from "styled-components";
 
 const FormUserDetails = () => {
   return (
-    <div>
-      <h1>FormUserDetails</h1>
-    </div>
+    <UserFormDetailsContainer>
+      <UserFormDetailsForm>
+        <FirstNameLabel>First Name</FirstNameLabel>
+        <FirstNameInput type="text" placeholder="Enter Your FirstName" />
+        <LastNameLabel>Last Name</LastNameLabel>
+        <LastNameInput type="text" placeholder="Enter Your LastName" />
+        <EmailLabel>Email</EmailLabel>
+        <EmailInput type="email" placeholder="Enter Your Email" />
+      </UserFormDetailsForm>
+    </UserFormDetailsContainer>
   );
 };
 
 export default FormUserDetails;
+
+const UserFormDetailsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+`;
+
+const UserFormDetailsForm = styled.form`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FirstNameLabel = styled.label`
+  width: 100%;
+  color: darkgray;
+  padding-bottom: 5px;
+`;
+
+const FirstNameInput = styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid black;
+  padding: 2px;
+  margin-bottom: 20px;
+  font-size: 1.2rem;
+`;
+
+const LastNameLabel = styled.label`
+  width: 100%;
+  color: darkgray;
+  padding-bottom: 5px;
+`;
+
+const LastNameInput = styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid black;
+  padding: 2px;
+  margin-bottom: 20px;
+  font-size: 1.2rem;
+`;
+
+const EmailLabel = styled.label`
+  width: 100%;
+  color: darkgray;
+  padding-bottom: 5px;
+`;
+
+const EmailInput = styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid black;
+  padding: 2px;
+  margin-bottom: 20px;
+  font-size: 1.2rem;
+`;
